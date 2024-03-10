9bc40f334a616a7e3cc453153959a68b2a7ac456
parkpoomd <parkpoom.d@gmail.com>
2024-03-10 12:16:41 +0700

    comit code

---
+++ src/components/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProvider,
+} from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SessionProvider>{children}</SessionProvider>
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
